be37d8111dfd7c1cf4841b0c687afec3dd328154
Jonas Bernoulli <jonas@bernoul.li>
2012-08-01 16:02:46 +0200

    untabify

--- save-local-vars.el
+++ save-local-vars.el
@@ -64,90 +64,90 @@ when the variable section is first created.")
       (goto-char (point-max))
       (search-backward "\n\^L" (max (- (point-max) 3000) (point-min)) 'move)
       (when (re-search-forward
-	     (format "^\\(%s+\\)[ \t]*Local Variables:\\(.*\\)$"
-		     (regexp-quote comment-start))
-	     nil t)
+             (format "^\\(%s+\\)[ \t]*Local Variables:\\(.*\\)$"
+                     (regexp-quote comment-start))
+             nil t)
         (let* ((prefix (match-string 1))
-	       (suffix (match-string 2))
-	       (begpos (line-end-position))
-	       (endpos
-		(when (re-search-forward (format "^%s[ \t]*End:[ \t]*%s$"
-						 (regexp-quote prefix)
-						 (regexp-quote suffix))
-					 nil t)
-		  (line-beginning-position))))
-	  (if endpos
-	      (list begpos endpos prefix suffix)
-	    (error "Local variables list is not properly terminated")))))))
+               (suffix (match-string 2))
+               (begpos (line-end-position))
+               (endpos
+                (when (re-search-forward (format "^%s[ \t]*End:[ \t]*%s$"
+                                                 (regexp-quote prefix)
+                                                 (regexp-quote suffix))
+                                         nil t)
+                  (line-beginning-position))))
+          (if endpos
+              (list begpos endpos prefix suffix)
+            (error "Local variables list is not properly terminated")))))))
 
 (defun save-local-variable--print (variable)
   (insert comment-start
-	  (if (string-match " $" comment-start) "" " ")
-	  (symbol-name variable) ": "
-	  (prin1-to-string (symbol-value variable))
-	  comment-end "\n"))
+          (if (string-match " $" comment-start) "" " ")
+          (symbol-name variable) ": "
+          (prin1-to-string (symbol-value variable))
+          comment-end "\n"))
 
 (defun save-local-variable (&optional variable)
   "Save the current buffer-local value of VARIABLE in visited file."
   (interactive
    (let* ((v (variable-at-point))
-	  (enable-recursive-minibuffers t)
-	  (val (completing-read (if (and (symbolp v)
-					 (local-variable-p v))
-				    (format
-				     "Save variable (default %s): " v)
-				  "Save variable: ")
-				obarray
-				'(lambda (vv)
-				   (and (boundp vv)
-					(local-variable-p vv)))
-				t nil nil
-				(if (and (symbolp v)
-					 (local-variable-p v))
-				    (symbol-name v)))))
+          (enable-recursive-minibuffers t)
+          (val (completing-read (if (and (symbolp v)
+                                         (local-variable-p v))
+                                    (format
+                                     "Save variable (default %s): " v)
+                                  "Save variable: ")
+                                obarray
+                                '(lambda (vv)
+                                   (and (boundp vv)
+                                        (local-variable-p vv)))
+                                t nil nil
+                                (if (and (symbolp v)
+                                         (local-variable-p v))
+                                    (symbol-name v)))))
      (list (if (equal val "") v
-	     (intern val)))))
+             (intern val)))))
   (unless (buffer-file-name)
     (error "Buffer isn't visiting a file: %s" (buffer-name)))
   (let ((standard-output (current-buffer))
-	(modifiedp (buffer-modified-p))
-	(data (save-local-variable--variables-data)))
+        (modifiedp (buffer-modified-p))
+        (data (save-local-variable--variables-data)))
     (save-excursion
       (if data
-	  (let ((begpos (nth 0 data))
-		(endpos (nth 1 data))
-		(comment-start (nth 2 data))
-		(comment-end   (nth 3 data))
-		varpos)
-	    (goto-char begpos)
-	    (if (re-search-forward
-		 (format "^%s *%s:"
-			 (regexp-quote comment-start)
-			 (regexp-quote (symbol-name variable)))
-		 endpos t)
-		(progn (forward-line 0) (kill-line))
-	      (goto-char endpos))
-	    (save-local-variable--print variable))
-	(if (re-search-forward (format "^%s+ .+ ends here"
-				       (regexp-quote comment-start))
-			       nil t)
-	    (forward-line 0)
-	  (goto-char (buffer-end 1))
-	  (insert "\n\^L"))
-	(unless (looking-back "^(provide '.+)\n")
-	  (insert "\n"))
-	(let ((comment-start
-	       (concat comment-start
-		       (and (string-match
-			     save-local-variable-double-comment-start
-			     comment-start)
-			    comment-start)
-		       (and (not (string-match " $" comment-start)) " "))))
-	  (insert comment-start "Local Variables:" comment-end "\n")
-	  (save-local-variable--print variable)
-	  (insert comment-start "End:" comment-end "\n"))))
+          (let ((begpos (nth 0 data))
+                (endpos (nth 1 data))
+                (comment-start (nth 2 data))
+                (comment-end   (nth 3 data))
+                varpos)
+            (goto-char begpos)
+            (if (re-search-forward
+                 (format "^%s *%s:"
+                         (regexp-quote comment-start)
+                         (regexp-quote (symbol-name variable)))
+                 endpos t)
+                (progn (forward-line 0) (kill-line))
+              (goto-char endpos))
+            (save-local-variable--print variable))
+        (if (re-search-forward (format "^%s+ .+ ends here"
+                                       (regexp-quote comment-start))
+                               nil t)
+            (forward-line 0)
+          (goto-char (buffer-end 1))
+          (insert "\n\^L"))
+        (unless (looking-back "^(provide '.+)\n")
+          (insert "\n"))
+        (let ((comment-start
+               (concat comment-start
+                       (and (string-match
+                             save-local-variable-double-comment-start
+                             comment-start)
+                            comment-start)
+                       (and (not (string-match " $" comment-start)) " "))))
+          (insert comment-start "Local Variables:" comment-end "\n")
+          (save-local-variable--print variable)
+          (insert comment-start "End:" comment-end "\n"))))
     (when (or (not modifiedp)
-	      (y-or-n-p (format "Save %s? " (buffer-file-name))))
+              (y-or-n-p (format "Save %s? " (buffer-file-name))))
       (save-buffer))))
 
 (provide 'save-local-vars)
